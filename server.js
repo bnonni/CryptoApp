755ce7b4c67f949e263704a7671a331b25bfec51
bnonni <bryanwnonni@gmail.com>
2019-06-04 13:40:32 -0400

    commiting change

--- server.js
+++ server.js
@@ -6,16 +6,25 @@ const app = express();
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://hu5ky5n0w:MongoDB4243!!@cryptowallet-glvp8.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const mongo = new MongoClient(uri, { useNewUrlParser: true });
 var db;
 
-client.connect(err => {
+mongo.connect(err => {
   let port = 3000;  
   if (err) return console.log(err);
-  db = client.db("crypto_wallet");
+  db = mongo.db("crypto_wallet");
   
   app.listen(port, () => {
-    console.log("Server running. Listening on port " + port);
+    console.log("Server listening on port " + port + ".");
+  });
+});
+
+app.post("/transactions", (req, res) => {
+  db.collection("transactions").insertOne(req.body, (err, result) => {
+    if (err) return console.log(err);
+    console.log("Saved to database");
+
+    res.redirect("/");
   });
 });
 
@@ -28,16 +37,17 @@ app.use(express.static(__dirname + '../public'));
 app.get('/', (req, res) => {
   db.collection("transactions").find().toArray(function(err, results){
     if (err) return console.log(err);
-    res.render('index.ejs', {transactions: results})
+    res.render('index.ejs', {transactions: results});
 
   });
 });
 
-app.post("/transactions", (req, res) => {
-  db.collection("transactions").insertOne(req.body, (err, result) => {
-    if (err) return console.log(err);
-    console.log("Saved to database");
 
-    res.redirect("/");
-  });
+var Client = require('coinbase').Client;
+var client = new Client({'apiKey': 'RusruCA3DYsReba5',
+                         'apiSecret': 'API SECRET'});
+
+client.getAccounts({}, function(err, accounts) {
+  // account.getTransactions(function(err, txs) {console.log(txs);});\
+  console.log(accounts);
 });
