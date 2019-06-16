d30b3d77c111f4b9f9d3152b7d1dd911004f0373
bryan.nonni <bryanwnonni@gmail.com>
2019-06-15 21:22:37 -0400

    commiting changes

--- server.js
+++ server.js
@@ -25,11 +25,6 @@ let port = 443;
 app.listen(port, () => {
   console.log("Server listening on port " + port);
  });
-
- /**
-  * Global Time Variables
-  */
- 
  /**
   * Render HP
   */
@@ -191,7 +186,7 @@ function calcBtcRSI14 () {
   var btc_prices_log = [];
   var j = 0;
   for(var i = btc_tickers.length - 1; i >= 0 ; i--){
-    if(btc_tickers[i] != undefined){
+    if(btc_tickers[i] != undefined && i%5==0){
       btc_prices.push(btc_tickers[i].price);
       if(j < 5){
         btc_prices_log.push(btc_tickers[j].price);
@@ -254,7 +249,7 @@ function calcBtcRSI14 () {
     var ltc_prices_log = [];
     var j = 0;
     for(var i = ltc_tickers.length - 1; i >= 0 ; i--){
-      if(ltc_tickers[i] != undefined){
+      if(ltc_tickers[i] != undefined  && i%5==0){
         ltc_prices.push(ltc_tickers[i].price);
         if(j < 5){
           ltc_prices_log.push(ltc_tickers[j].price);
@@ -317,7 +312,7 @@ function calcBtcRSI14 () {
       var eth_prices_log = [];
       var j = 0;
       for(var i = eth_tickers.length - 1; i >= 0; i--){
-        if(eth_tickers[i] != undefined){
+        if(eth_tickers[i] != undefined  && i%5==0){
           eth_prices.push(eth_tickers[i].price);
           if(j < 5){
             eth_prices_log.push(eth_tickers[j].price);
