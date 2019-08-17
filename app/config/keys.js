f9eb7b63da3ab386406b3c59201cd3953976d352
bnonni <bryanwnonni@gmail.com>
2019-08-17 12:11:16 -0700

    change DB uri to localhost

--- app/config/keys.js
+++ app/config/keys.js
@@ -1,8 +1,8 @@
 // var user = process.env.user;
 // var password = process.env.MongoPassword;
 
-module.exports = {
- // MongoURI: "mongodb+srv://" + user + ":" + password + "@cryptowallet-glvp8.mongodb.net/",
- MongoURI = "mongodb://localhost:27017/crypto_wallet",
- secretOrKey: "secret"
-}
+// module.exports = {
+//  // MongoURI: "mongodb+srv://" + user + ":" + password + "@cryptowallet-glvp8.mongodb.net/",
+//  MongoURI = "mongodb://localhost:27017/crypto_wallet",
+//  secretOrKey: "secret"
+// }
