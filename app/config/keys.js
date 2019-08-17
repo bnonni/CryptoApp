fdf7a327c319596de32c03e5a8b2d2865602fc22
bnonni <bryanwnonni@gmail.com>
2019-08-17 12:04:19 -0700

    local mongoDB setup for dev

--- app/config/keys.js
+++ app/config/keys.js
@@ -1,9 +1,8 @@
-var user = process.env.user;
-var password = process.env.MongoPassword;
+// var user = process.env.user;
+// var password = process.env.MongoPassword;
 
 module.exports = {
- // : "mongodb+srv://" + + ":" + ,
- MongoURI: "mongodb+srv://" + user + ":" + password + "@cryptowallet-glvp8.mongodb.net/",
-
+ // MongoURI: "mongodb+srv://" + user + ":" + password + "@cryptowallet-glvp8.mongodb.net/",
+ MongoURI = "mongodb://localhost:27017/crypto_wallet",
  secretOrKey: "secret"
 }
