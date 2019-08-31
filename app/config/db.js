5b0133ae891ddfe7a502994918699979f6d82914
bnonni <bryanwnonni@gmail.com>
2019-08-31 13:22:37 -0400

    commiting local changes

--- app/config/db.js
+++ app/config/db.js
@@ -1,14 +1,15 @@
 const MongoClient = require("mongodb").MongoClient;
-const user = process.env.user;
+const user = "admin";
 const password = process.env.password;
 //mongodb://192.241.152.52:27017/
-const MongoURI = "mongodb://" + user + ":" + password + "192.241.152.52:27017";
+const MongoURI = "mongodb://admin:" + password + "192.241.152.52:27017/crypto_wallet";
 // "mongodb://localhost:27017/crypto_wallet"
 var _db;
 
 module.exports = {
  connectToServer: function (callback) {
   MongoClient.connect(MongoURI, { useNewUrlParser: true }, (err, client) => {
+   console.log(client);
    _db = client.db("crypto_wallet");
    console.log("Connected to " + _db);
    return callback(err);
