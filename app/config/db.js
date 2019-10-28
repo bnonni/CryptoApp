3a2980ce5d73304d9ba2dd9235f2870107bf956e
bnonni <bryanwnonni@gmail.com>
2019-10-28 15:54:06 -0400

    commiting local changes

--- app/config/db.js
+++ app/config/db.js
@@ -1,18 +1,18 @@
 /*jshint esversion: 6 */
-
+const mongo_admin_pwd = process.env.db_pwd;
 const MongoClient = require("mongodb").MongoClient;
-const MongoURI = "mongodb://localhost:27017/crypto_wallet"
+const MongoURI = "mongodb://bryan:" + mongo_admin_pwd + "/crypto_wallet"
 var _db;
 
 module.exports = {
- connectToServer: function (callback) {
-  MongoClient.connect(MongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-   _db = client.db("crypto_wallet");
-   return callback(err);
-  });
- },
+    connectToServer: function(callback) {
+        MongoClient.connect(MongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+            _db = client.db("crypto_wallet");
+            return callback(err);
+        });
+    },
 
- getDb: function () {
-  return _db;
- }
+    getDb: function() {
+        return _db;
+    }
 };
