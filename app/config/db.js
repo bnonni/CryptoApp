f0813c28ba0a6129c3d71f530bdc69e2b3c7ae61
Bryan Nonni <bryanwnonni@gmail.com>
2019-10-31 00:56:25 +0000

    db changes

--- app/config/db.js
+++ app/config/db.js
@@ -1,13 +1,7 @@
 /*jshint esversion: 6 */
-<<<<<<< HEAD
 const AdminPwd = process.env.MongoAdminPwd;
 const MongoClient = require("mongodb").MongoClient;
 const MongoURI = "mongodb://CryptoMongoAdmin:"+AdminPwd+":@localhost:27017/crypto_wallet"
-=======
-const mongo_admin_pwd = process.env.db_pwd;
-const MongoClient = require("mongodb").MongoClient;
-const MongoURI = "mongodb://bryan:" + mongo_admin_pwd + "/crypto_wallet"
->>>>>>> 74593593da2aff7195dc6a494c87482b5b9b9d51
 var _db;
 
 module.exports = {
@@ -18,14 +12,7 @@ module.exports = {
         });
     },
 
-<<<<<<< HEAD
  getDb: function () {
   return _db;
  }
 };
-=======
-    getDb: function() {
-        return _db;
-    }
-};
->>>>>>> 74593593da2aff7195dc6a494c87482b5b9b9d51
